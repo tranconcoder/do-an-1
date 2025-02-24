c9ae45348a6ccb1cf7504ec81889f52a919d007b
tvconss <tranconcoder@gmail.com>
2025-02-24 15:47:05 +0700

    add readme.md

---
+++ readme.md
@@ -0,0 +1,30 @@
+# Những kiến thức đạt được trong quá trình làm dự án
+
+## Những packages cần thiết
+
+### Nhóm trợ dev thuận tiện
+
+1. Morgan: Logger trên terminal
+2. Nodemon: lắng nghe và reload máy chủ (không cần nếu dùng tsx, node bản mới)
+3. Tsx (typescript excute): hỗ trợ auto compile typescript code và tích hợp giống nodemon
+4. Http status codes: cung cấp các thông tin có sẵn về http status code
+
+### Server core
+
+1. Express: framework chính hỗ trợ phát triển routing cho máy chủ
+2. Compression: hỗ trợ nén dữ liệu trong quá trình truyền
+3. Helmet: bảo mật cho máy chủ
+4. PM2: hỗ trợ quản lý máy chủ trong môi trường production
+5. Winston: hệ thống logger file, hỗ trợ nhiều mức độ log
+6. Winston daily rotate file: logge file theo ngày, tuần, tháng, năm
+7. Json web token: token hỗ trợ xác thực và phân quyền
+8. Mongoose, mongodb: cơ sở dữ liệu NoSQL
+
+### Nhóm công cụ phát triển
+
+1. Cron: tạo luồng child_process và xử lý tác vụ lập lịch đã lập
+2. Dotenv: ẩn thông tin nhạy cảm trong source code
+3. Joi: validate dữ liệu trước khi xử lý
+4. Joi extract type: lấy type của schema đã tạo trong typescript
+5. Lodash: công cụ tương tác object, array
+6. UUID: tạo uuid ngẫu nhiên cho hệ thống.
--- src/api/services/keyToken.service.ts
+++ src/api/services/keyToken.service.ts
@@ -1,9 +1,17 @@
-import crypto from 'crypto';
-import keyTokenModel, { KeyTokenModel } from '../models/keyToken.model';
-import { ObjectAnyKeys } from '../types/object';
-import mongoose from 'mongoose';
 import { SaveKeyTokenArgs, SaveNewJwtTokenArgs } from '../types/keyToken';
+import { ObjectAnyKeys } from '../types/object';
+
+// Libs
+import crypto from 'crypto';
+import mongoose from 'mongoose';
+
+// Models
+import keyTokenModel, { KeyTokenModel } from '../models/keyToken.model';
+
 export default class KeyTokenService {
+	/* ===================================================== */
+	/*                  GET TOKEN BY USER ID                 */
+	/* ===================================================== */
 	public static getTokenByUserId = async (
 		userId: string | mongoose.Types.ObjectId
 	): Promise<KeyTokenModel | ObjectAnyKeys> => {
@@ -12,6 +20,9 @@ export default class KeyTokenService {
 		return keyToken ? keyToken : {};
 	};
 
+	/* ===================================================== */
+	/*            SAVE NEW KEY TOKEN WHEN SIGN UP            */
+	/* ===================================================== */
 	public static saveKeyToken = async ({
 		userId,
 		privateKey,
@@ -30,23 +41,20 @@ export default class KeyTokenService {
 		return keyToken._id;
 	};
 
+	/* ===================================================== */
+	/*           SAVE NEW TOKEN GENERATED ON LOGIN           */
+	/* ===================================================== */
 	public static saveNewJwtToken = async ({
 		userId,
 		accessToken,
 		refreshToken,
 	}: SaveNewJwtTokenArgs) => {
 		const updateResult = await keyTokenModel.updateOne(
-			{
-				user: userId,
-			},
+			{ user: userId },
 			{
 				$push: {
-					access_tokens: {
-						token: accessToken,
-					},
-					refresh_tokens: {
-						token: refreshToken,
-					},
+					access_tokens: { token: accessToken },
+					refresh_tokens: { token: refreshToken },
 				},
 			}
 		);
@@ -54,6 +62,9 @@ export default class KeyTokenService {
 		return updateResult.modifiedCount > 0;
 	};
 
+	/* ===================================================== */
+	/*             GENERATE TOKEN PAIR ON SIGN UP            */
+	/* ===================================================== */
 	public static generateTokenPair = () => {
 		return crypto.generateKeyPairSync('rsa', {
 			modulusLength: 4096,
